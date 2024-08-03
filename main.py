ac1c4aef4becf4de240c6d0a45376d3c62914a54
Dario Dematties <djdematties@gmail.com>
2024-08-03 09:29:49 -0500

    Partial commit

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ import requests
 import torch
 from PIL import Image
 from transformers import AutoProcessor, AutoModelForCausalLM 
-from source.utils import run_example
+from source.utils import get_reward_from_image
 
 def main():
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    #device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    #torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    device = "cpu"
+    torch_dtype = torch.float32
 
     model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-large", torch_dtype=torch_dtype, trust_remote_code=True).to(device)
     processor = AutoProcessor.from_pretrained("microsoft/Florence-2-large", trust_remote_code=True)
 
-    url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg?download=true"
-    image = Image.open(requests.get(url, stream=True).raw)
+    #url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg?download=true"
+    #image = Image.open(requests.get(url, stream=True).raw)
 
-    prompt = "<MORE_DETAILED_CAPTION>"
-    answer = run_example(processor, image, device, torch_dtype, model, prompt)['<MORE_DETAILED_CAPTION>']
+    #prompt = "<MORE_DETAILED_CAPTION>"
+    #answer = run_example(processor, image, device, torch_dtype, model, prompt)['<MORE_DETAILED_CAPTION>']
 
-    task_prompt = "<CAPTION_TO_PHRASE_GROUNDING>"
-    results = run_example(processor, image, device, torch_dtype, model, task_prompt, text_input=answer)
+    #task_prompt = "<CAPTION_TO_PHRASE_GROUNDING>"
+    #results = run_example(processor, image, device, torch_dtype, model, task_prompt, text_input=answer)
 
-    print(results)
+    #print(results)
+
+
 
   
   
--- source/utils.py
+++ source/utils.py
@@ -15,3 +15,44 @@ def run_example(processor, image, device, torch_dtype, model, task_prompt, text_
     parsed_answer = processor.post_process_generation(generated_text, task=task_prompt, image_size=(image.width, image.height))
 
     return parsed_answer
+
+
+
+
+def bbox_area(bbox):
+    x1=bbox[0]
+    y1=bbox[1]
+    x2=bbox[2]
+    y2=bbox[3]
+    assert x1 < x2
+    assert y1 < y2
+    x_len = x2-x1
+    y_len = y2-y1
+    return x_len*y_len
+
+
+
+
+def get_reward_from_bboxes(bboxes, image):
+    ratios = []
+    for bbox in bboxes:
+        ratios.append(bbox_area(bbox)/np.prod(image.size))
+
+    return sum(ratios)/len(ratios)
+
+
+
+
+def get_reward_from_image(processor, image, device, torch_dtype, model, task_prompt, text_input=None):
+    task_prompt = "<MORE_DETAILED_CAPTION>"
+    caption=run_example(processor, image, device, torch_dtype, model, task_prompt, text_input)[prompt]
+    task_prompt = "<CAPTION_TO_PHRASE_GROUNDING>"
+    results = run_example(processor, image, device, torch_dtype, model, task_prompt, text_input)
+    bboxes=results['<CAPTION_TO_PHRASE_GROUNDING>']['bboxes']
+    labels=results['<CAPTION_TO_PHRASE_GROUNDING>']['labels']
+
+    return get_reward_from_bboxes(bboxes, image)
+
+
+
+
